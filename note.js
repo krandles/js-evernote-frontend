bc998fec82b78646476d621b5f2bb426216c5c2f
krandles <krandles@gmail.com>
2018-02-08 00:03:51 -0500

    slightly better style

--- note.js
+++ note.js
@@ -9,12 +9,13 @@ const Note = (function () {
 
     renderHTML() {
       let noteListItem = document.createElement("li")
-      let noteListLink = document.createElement("a")
-      let noteListUrl = `#`
-      noteListLink.href = noteListUrl
-      let noteListText = document.createTextNode(this.title)
-      noteListLink.append(noteListText)
-      noteListItem.append(noteListLink)
+      // let noteListLink = document.createElement("a")
+      // let noteListUrl = `#`
+      // noteListLink.href = noteListUrl
+      // let noteListText = document.createTextNode(this.title)
+      noteListItem.innerHTML = `<a href="#" data-id=${this.id} on-click="${this.renderNoteHTML()}">${this.title}</a>`
+      // noteListLink.append(noteListText)
+      // noteListItem.append(noteListLink)
       return noteListItem
     }
 
@@ -23,13 +24,14 @@ const Note = (function () {
     }
 
     renderNoteHTML() {
+      debugger
       let noteContainer = document.createElement('div')
       let noteTitle = document.createElement('h3')
       let noteBody = document.createElement('p')
       let noteTitleText = document.createTextNode(this.title)
       let noteBodyText = document.createTextNode(this.body)
       noteTitle.append(noteTitleText)
-      noteBody.append(noteBodyText)
+      noteBody.innerHTML = this.body.replace(/\\n/g, "<br />")
       noteContainer.append(noteTitle)
       noteContainer.append(noteBody)
       return noteContainer
